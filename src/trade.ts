c73ba0d54a01990e320b4fdf00fd9de84dfb14e5
Rudra Prasad <rudraofficial202@gmail.com>
2025-05-06 10:50:43 +0530

    added exit position

--- src/trade.ts
+++ src/trade.ts
@@ -1,7 +1,6 @@
 import { KiteConnect } from "kiteconnect";
 import dotenv from "dotenv";
 import { TradeData } from "./message";
-
 dotenv.config();
 
 const kc = new KiteConnect({
@@ -15,7 +14,7 @@ export async function initZerodha(): Promise<void> {
       process.env.KITE_API_SECRET!
     );
     kc.setAccessToken(res.access_token);
-    console.log("Zerodha session initialized.");
+    console.log("Zerodha session initialized. waiting for Message...");
   } catch (err) {
     console.error("Zerodha session error:", err);
   }
@@ -25,18 +24,40 @@ export async function placeOrder(data: TradeData): Promise<void> {
   const tradingSymbol = `${data.symbol}25${data.expiry}${data.strike}${data.optionType}`;
   console.log("Generated Trading Symbol: ", tradingSymbol);
 
+  const numberOfLots = 4;
+  const lotSize = data.lotSize!;
+  const buyQty = numberOfLots * lotSize;
+  const sellQty = (numberOfLots - 1) * lotSize;
+
   try {
     await kc.placeOrder("regular", {
       exchange: "NFO",
       tradingsymbol: tradingSymbol,
       transaction_type: "BUY",
-      quantity: data.lotSize!,
+      quantity: buyQty,
       product: "NRML",
       order_type: "LIMIT",
       price: data.range?.[1] || data.cmp!,
     });
     console.log("Placing order with data:", data);
     console.log("✅ Order placed for", tradingSymbol);
+    console.log(`✅ Entered ${numberOfLots} lots of ${tradingSymbol}`);
+
+    setTimeout(async () => {
+      try {
+        await kc.placeOrder("regular", {
+          exchange: "NFO",
+          tradingsymbol: tradingSymbol,
+          transaction_type: "SELL",
+          quantity: sellQty,
+          product: "NRML",
+          order_type: "MARKET",
+        });
+        console.log(`⏳ Exited ${numberOfLots - 1} lots after 3.5 minutes`);
+      } catch (err: any) {
+        console.error("❌ Exit order failed:", err.message);
+      }
+    }, 210000); // 3.5 minutes
   } catch (err: any) {
     console.error("❌ Order placement failed:", err.message);
   }
